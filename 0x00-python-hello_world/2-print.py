14c1f0e3a26536560fce1733f425086f965797e9
abdi430 <abdibale9@gmail.com>
2023-11-30 07:48:50 -0800

    00x

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase, not followed by a new line."""
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+print("\"Programming is like building a multilingual puzzle")
