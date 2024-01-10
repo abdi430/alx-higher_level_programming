5467fa3d526ee6fe5705202e1b711fc6a2e14b2e
abdi430 <abdibale9@gmail.com>
2024-01-10 03:14:05 -0800

    tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+class TestMaxInteger(unittest.TestCase):
+    """Tests for the max_integer function"""
+    def setUp(self):
+        self.ordered_list = [1, 2, 3, 4]
+        self.unordered_list = [1, 3, 4, 2]
+
+    def test_maxs(self):
+        self.assertEqual(max_integer(self.ordered_list),
+                         max_integer(self.unordered_list))
+
+    def test_string(self):
+        self.assertEqual(max_integer("string"), 't')
+
+    def test_nothing(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_list_of_negatives(self):
+        self.assertEqual(max_integer([-1, -99, -54]), -1)
+
+    def test_key_error(self):
+        with self.assertRaises(KeyError):
+            max_integer({5 : 6})
+
+    def test_type_error_too_many_args(self):
+        with self.assertRaises(TypeError):
+            max_integer('tuple', 'ofstrings')
+
+    def test_type_error_on_incomparable_list_items(self):
+        with self.assertRaises(TypeError):
+            max_integer([[], {}])
