e21025afd8111a10e99736808f563a26b284fa0c
abdi430 <abdibale9@gmail.com>
2024-03-20 22:47:42 -0700

    python object  relational Mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,25 +5,24 @@ script that lists all cities from the database hbtn_0e_4_usa
 ============================================================
 """
 
--- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
-USE hbtn_0e_4_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+import MySQLdb
+import sys
 
-CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+
+def states():
+    """states to import"""
+
+    connect_db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                 passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cur = connect_db.cursor()
+    que = "SELECT cities.id, cities.name, states.name FROM cities INNER\
+           JOIN states ON cities.state_id = states.id;"
+    cur.execute(que)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    connect_db.close()
+if __name__ == "__main__":
+    states()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,21 +5,23 @@ script that prints the first State object from the database hbtn_0e_6_usa
 =========================================================================
 """
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
 import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-    the_session = sessionmaker(bind=engine)
-    session = the_session()
-
-    el_primerito = session.query(State).first()
-    if el_primerito is None:
-        print("Nothing")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if (state):
+        print("{}: {}".format(state.id, state.name))
     else:
-        print("{}: {}".format(el_primerito.id, el_primerito.name))
+        print("Nothing")
+    session.close()
