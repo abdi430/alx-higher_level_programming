ff89ace405a6518edc2560b1e5c049e756f5a22d
abdi430 <abdibale9@gmail.com>
2024-03-21 06:40:42 -0700

    pyth i/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@ def read_file(filename=""):
     """function that reads a text file and print"""
 
     def read_file(filename=""):
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
+        with open(filename, 'r', encoding="utf-8") as f:
+            for line in f:
+                print(line, end='')
