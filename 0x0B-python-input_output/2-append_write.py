07a78ac108c0be63f1660fa8914e7c5fc71d6059
abdi430 <abdibale9@gmail.com>
2024-03-22 01:46:26 -0700

    python I/O

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    """function that reads n lines of a text file and prints it to stdout"""
-
+def append_write(filename="", text=""):
     lines = 0
-    with open(filename) as f:
-        for lin in f:
-            lines += 1
-        f.seek(0)
-        if nb_lines <= 0 or nb_lines >= lines:
-            print(f.read(), end="")
-        else:
-            for i in range(0, nb_lines):
-                print(f.readline(), end="")
+    with open(filename, 'a') as f:
+        lines = f.write(text)
+    return lines
