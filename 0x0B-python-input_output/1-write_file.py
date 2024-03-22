41c4a6b15fa417ce6fc2b68348658524ea958dcf
abdi430 <abdibale9@gmail.com>
2024-03-22 01:40:03 -0700

    python I/O

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    """function that reads a text and return number of lines"""
-
+def write_file(filename="", text=""):
     lines = 0
-    with open(filename) as f:
-        for lin in f:
-            lines += 1
-        return (lines)
+    with open(filename, 'w') as f:
+        lines = f.write(text)
+    return lines
