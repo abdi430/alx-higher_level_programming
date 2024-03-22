d04a68c8d13234802a5a7b59c4121778b4a8bc4f
abdi430 <abdibale9@gmail.com>
2024-03-22 01:37:29 -0700

    python I/O

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" string to josn"""
-def write_file(filename="", text=""):
-    """function that writes a string to a text file and return the number
-    of characters written"""
+import json
+""" json to string """
 
-    with open(filename, 'w') as f:
-        return f.write(text)
+def to_json_string(my_obj):
+    data = json.dumps(my_obj)
+    return data
