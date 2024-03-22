d0b70e26c885851cc9f903c5c19dc8e5e92b3cc7
abdi430 <abdibale9@gmail.com>
2024-03-22 01:26:28 -0700

    python I/O

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+
+
 def number_of_lines(filename=""):
-    lines = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            lines += 1
-    return lines
+    with open(filename, "r", encoding="UTF-8") as f:
+        return len(list(f))
